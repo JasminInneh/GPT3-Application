381f3abb5d0be148831405c52b616a37e076c014
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2024-03-06 01:32:04 +0000

    features container complete

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -29,7 +29,7 @@ const Features = () => {
         </div>
         <div className='gpt3__features-container'>
           {featuresData.map((item, index) => (
-            <Feature title={item.item} text={item.text} key={item.title + index}/>
+            <Feature title={item.title} text={item.text} key={item.title + index}/>
             // The line above is where you left off last
           ))}
 
